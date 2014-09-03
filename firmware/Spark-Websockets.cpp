cc661723c08967daf5d43360e490cc6b93dd9faa
ekbduffy <ekbduffy@gmail.com>
2014-09-03 18:46:32 +0600

    Update Spark-Websockets.cpp

--- firmware/Spark-Websockets.cpp
+++ firmware/Spark-Websockets.cpp
@@ -68,7 +68,7 @@
 #include "spark_wiring_usbserial.h"
 #include "spark_wiring_string.h"
 
-#include "Spark-Websockets/Spark-Websockets.h"
+#include "Spark-Websockets.h"
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdlib.h>
